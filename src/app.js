891f74d3c9fbd2e80ed294cd1867949e3a9114d6
franvilares <fvilares84@gmail.com>
2023-07-05 19:01:25 +0000

    ejercicio random card

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,44 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suitList = ["♣", "♦", "♥", "♠"];
+  const redElements = ["♦", "♥"];
+  const numbersList = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  generateCard(suitList, numbersList, redElements);
+};
+
+const selectRandomItem = array => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+const generateCard = (suitList, numbersList, redElements) => {
+  const selectedSuit = selectRandomItem(suitList);
+  const selectedNumbersList = selectRandomItem(numbersList);
+
+  const numberDomElement = document.getElementById("card-number");
+  numberDomElement.textContent = selectedNumbersList;
+
+  const isRed = redElements.includes(selectedSuit);
+
+  const suitDomElements = document.querySelectorAll(".card-suit");
+
+  suitDomElements.forEach(elem => {
+    elem.textContent = selectedSuit;
+    elem.style.color = isRed ? "red" : "black";
+  });
 };
